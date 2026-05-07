679da24d02c61138d554fa379cc46bf85a4e2939
Jaylou Quigao <jaylouquigao3@gmail.com>
2026-05-08 05:15:00 +0800

    Configure database connection

--- config/db.php
+++ config/db.php
@@ -4,6 +4,7 @@ $dbname = "sporthub";
 $username = "root";
 $password = "";
 
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
