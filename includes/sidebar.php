384900ebd5cd69e7811f23350c3071c030041ee2
Jaylou Quigao <jaylouquigao3@gmail.com>
2026-05-14 05:30:00 +0800

    Add sidebar navigation

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,6 @@
 <div class="bg-light p-3" style="width:200px;min-height:100vh;">
 <h4>Menu</h4>
+
 <ul class="nav flex-column">
 <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
 <li class="nav-item"><a class="nav-link" href="inventory.php">Inventory</a></li>
